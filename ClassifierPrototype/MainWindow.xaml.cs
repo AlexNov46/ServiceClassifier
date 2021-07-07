bab37b7d76a2b55e52ee5b6da5d5b1e4cb44a9f6
Novikov Alexander <pahnum@yandex.ru>
2021-07-07 16:58:48 +0300

    Нейронка
    Первая версия

--- ClassifierPrototype/MainWindow.xaml.cs
+++ ClassifierPrototype/MainWindow.xaml.cs
@@ -28,10 +28,42 @@ namespace ClassifierPrototype
             InitializeComponent();
         }
 
+        private void Prepare(ref string input)
+        {
+            input = input.Replace("ё", "е");
+            input = System.Text.RegularExpressions.Regex.Replace(input, @"[^a-zA-Zа-яА-Я1-9]+", " ");
+            input = System.Text.RegularExpressions.Regex.Replace(input, @"[ ]+", " ");
+            input = input.Trim();
+        }
+        private void Processing()
+        {
+            string input = Input.Text;
+            Prepare(ref input);
+            ModelInput inputText = new ModelInput()
+            {
+                Col0 = input,
+            };
+            var result = ConsumeModel.Predict(inputText);
+            if (Math.Abs(result.Score[0] - result.Score[1]) <= 0.15)
+            {
+                Output.Content = "Нейтральный!";
+            }
+            else if (result.Prediction == "-1 ")
+            {
+                Output.Content = "Отрицательный!";
+            }
+            else if (result.Prediction == "1 ")
+            {
+                Output.Content = "Положительный!";
+            }
+            Output.Visibility = Visibility.Visible;
+        }
+
         
         private void Start_Click(object sender, RoutedEventArgs e)
         {
-
+            Output.Visibility = Visibility.Hidden;
+            Processing();
         }
     }
 }

8e97e3f62749d1985ae45e10c4190b259c8e3c92
Novikov Alexander <pahnum@yandex.ru>
2021-07-14 15:46:22 +0300

    REST API Done!

--- ServiceClassifierWEB/ServiceClassifierWEB/Controllers/ClassificationController.cs
+++ ServiceClassifierWEB/ServiceClassifierWEB/Controllers/ClassificationController.cs
@@ -1,11 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ClassifierPrototypeML.Model;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.ML;
-using ClassifierPrototypeML.Model;
+using System;
 
 namespace ServiceClassifierWEB.Controllers
 {
@@ -18,57 +14,75 @@ namespace ServiceClassifierWEB.Controllers
         {
             Pool = _Pool;
         }
-        private void Prepare(ref string text)
+        private void Preprocessing(ref string text)
         {
-            text = text.Replace("ё", "е");
-            text = System.Text.RegularExpressions.Regex.Replace(text, @"((www\.[^\s]+)|(https?://[^\s]+))", " ");
-            text = System.Text.RegularExpressions.Regex.Replace(text, @"[^a-zA-Zа-яА-Я1-9]+", " ");
-            text = System.Text.RegularExpressions.Regex.Replace(text, @"[ ]+", " ");
-            text = text.Trim();
-            text = text.ToLower();
-        }
-        private string Process(string input)
-        {
-            string output = String.Empty;
-            Prepare(ref input);
-            if (String.IsNullOrEmpty(input))
+            if (!String.IsNullOrEmpty(text))
             {
-                output = "Neutral!";
+                text = text.Replace("ё", "е");
+                text = System.Text.RegularExpressions.Regex.Replace(text, @"((www\.[^\s]+)|(https?://[^\s]+))", " ");
+                text = System.Text.RegularExpressions.Regex.Replace(text, @"[^a-zA-Zа-яА-Я1-9]+", " ");
+                text = System.Text.RegularExpressions.Regex.Replace(text, @"[ ]+", " ");
+                text = text.Trim();
+                text = text.ToLower();
+            }
+        }
+        private OutputData Query(string input)
+        {
+            string result = String.Empty;
+            Preprocessing(ref input);
+            if (String.IsNullOrEmpty(input) || String.IsNullOrWhiteSpace(input))
+            {
+                result = "Empty Query!";
+            }
+            var load = new ModelInput
+            {
+                Col0 = input
+            };
+            var modelOut = Pool.Predict(load);
+            if (Math.Abs(modelOut.Score[0] - modelOut.Score[1]) <= 0.15)
+            {
+                result = "Neutral!";
+            }
+            else if (modelOut.Prediction == "-1 ")
+            {
+                result = "Negative!";
+            }
+            else if (modelOut.Prediction == "1 ")
+            {
+                result = "Positive!";
+            }
+
+            return (new OutputData { Output = result });
+
+        }
+
+        //Запрос в URL
+        [HttpGet("urlquery/{input}")]
+        public ActionResult<OutputData> GetFromURL(string input)
+        {
+            return Query(input);
+        }
+
+        //Пустой запрос URL
+        [HttpGet("urlquery")]
+        public ActionResult<OutputData> GetEmptyURL()
+        {
+            return (new OutputData { Output = "Empty Query!" });
+        }
+
+        //Запрос через формат JSON
+
+        [HttpGet("jsonquery")]
+        public ActionResult<OutputData> GetFromJSON([FromBody] InputData inputJSON)
+        {
+            if (String.IsNullOrEmpty(inputJSON.Input))
+            {
+                return (new OutputData { Output = "Empty Query" });
             }
             else
             {
-                var load = new ModelInput
-                {
-                    Col0 = input
-                };
-                var result = Pool.Predict(load);
-                if (Math.Abs(result.Score[0] - result.Score[1]) <= 0.15)
-                {
-                    output = "Neutral!";
-                }
-                else if (result.Prediction == "-1 ")
-                {
-                    output = "Negative!";
-                }
-                else if (result.Prediction == "1 ")
-                {
-                    output = "Positive";
-                }
+                return Query(inputJSON.Input);
             }
-            return output;
-            
         }
-
-        [HttpGet]
-        public ActionResult<string> Go([FromBody] string inputText)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest();
-            }
-            string output = Process(inputText);
-            return Ok(output);
-        }
-
     }
 }
